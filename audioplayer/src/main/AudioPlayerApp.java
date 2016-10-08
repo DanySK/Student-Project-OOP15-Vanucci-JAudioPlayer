368341470ade5524c10df3eaa91a694cad2ce93b
franux <franux@localhost>
2016-10-08 11:14:54 +0200

    - spostata la creazione del file nella classe di avvio

--- audioplayer/src/main/AudioPlayerApp.java
+++ audioplayer/src/main/AudioPlayerApp.java
@@ -2,6 +2,8 @@ package main;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
@@ -17,6 +19,8 @@ import model.FileHandler;
  */
 public class AudioPlayerApp {
 
+	private static final String USERS_FILE = "users.txt";
+	
 	public static void main(String[] args){
 		
 		if(!new File(FileHandler.getMainDir()).exists()){
@@ -26,6 +30,16 @@ public class AudioPlayerApp {
 				e.printStackTrace();
 			}
 		}
+		
+		String usersPath = FileHandler.getMainDir()+USERS_FILE;
+		if(!new File(usersPath).exists()){
+			try {
+				Files.copy(AudioPlayerApp.class.getResourceAsStream("/users/users.txt"), Paths.get(usersPath));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
 		try {
 			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 		} catch (Exception ex) {
